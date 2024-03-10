5d50a9d27ef8dad3056286b63ce0bdead1e00629
lzaidi <136109326+lzaidi@users.noreply.github.com>
2024-03-10 14:12:35 -0700

    Update dtree.py

--- dtree.py
+++ dtree.py
@@ -18,14 +18,13 @@ class DecisionNode:
 
 class LeafNode:
     def __init__(self, y, prediction):
-        "Create leaf node from y values and prediction; prediction is mean(y) or mode(y)"
+        "Creates leaf node from y values and prediction; prediction is mean(y) or mode(y)"
         self.n = len(y)
         self.prediction = prediction
-        self.y=y
 
     def predict(self, x_test):
         # return prediction
-        return self.y
+        return self.prediction
 
 
 def gini(y):
@@ -34,85 +33,70 @@ def gini(y):
     Assume y = {0,1}
     Gini = 1 - sum_i p_i^2 where p_i is the proportion of class i in y
     """
-    total=0
-    set_y=set(y)
+    total = 0
+    set_y = set(y)
     for p in set_y:
-        prop=((y==p).sum()/len(y))**2
-        total+=prop
-    return 1-total
-
+        prop = ((y == p).sum() / len(y)) ** 2
+        total += prop
+    return 1 - total
 
     
 def find_best_split(X, y, loss, min_samples_leaf):
-    best_feature=-1
-    best_split=-1
-    k=11
-    best_loss=loss(y)
-    random_columns=(np.random.choice(len(X.T), size=k))
-    for i in random_columns:
-        candidates=(np.random.choice(X[:,i], size=k))
+    best_feature = -1
+    best_split =- 1
+    k = 11
+    best_loss = loss(y)
+    for i in range(1,len(X.T)):
+        candidates = (np.random.choice(X[:, i], size=k))
         for split in candidates:
-            yl=y[X[:, i]<split]
-            yr=y[X[:, i]>=split]
+            yl = y[X[:, i]<split]
+            yr = y[X[:, i]>=split]
             if len(yl) < min_samples_leaf or len(yr) < min_samples_leaf:
                 continue
-            l = ((len(yl) * loss(yl)) + (len(yr)*loss(yr)))/(len(yl) + len(yr))
-            if l==0:
+            l = ((len(yl) * loss(yl)) + (len(yr) * loss(yr))) / (len(yl) + len(yr))
+            if l == 0:
                 return i, split 
             if l < best_loss:
-                best_loss=l
-                best_feature=i
-                best_split=split
+                best_loss = l
+                best_feature = i
+                best_split = split
     return best_feature, best_split 
 
 class DecisionTree621:
     def __init__(self, min_samples_leaf=1, loss=None):
         self.min_samples_leaf = min_samples_leaf
-        self.loss = loss # loss function; either np.var for regression or gini for classification
+        self.loss = loss 
         
     def fit(self, X, y):
         """
-        Create a decision tree fit to (X,y) and save as self.root, the root of
-        our decision tree, for  either a classifier or regression.  Leaf nodes for classifiers
-        predict the most common class (the mode) and regressions predict the average y
-        for observations in that leaf.
-
-        This function is a wrapper around fit_() that just stores the tree in self.root.
+        Creates a decision tree fit to (X,y) and save as self.root, the root of
+        our decision tree, for  either a classifier or regression.
         """
-        self.root = self.fit_( X, y)
+        self.root = self.fit_(X, y)
 
 
     def fit_(self, X, y):
         """
-        Recursively create and return a decision tree fit to (X,y) for
-        either a classification or regression.  This function should call self.create_leaf(X,y)
-        to create the appropriate leaf node, which will invoke either
-        RegressionTree621.create_leaf() or ClassifierTree621.create_leaf() depending
-        on the type of self.
-
-        This function is not part of the class "interface" and is for internal use, but it
-        embodies the decision tree fitting algorithm.
-
-        (Make sure to call fit_() not fit() recursively.)
+        Recursively creates and returns a decision tree fit to (X,y) for
+        either a classification or regression. 
         """
         if len(X) <= self.min_samples_leaf or len(np.unique(X))==1:
             return self.create_leaf(y)
         col, split = find_best_split(X, y, self.loss, self.min_samples_leaf)
         if col == -1:
             return self.create_leaf(y)
-        lchild=self.fit_(X[X[:, col]<split], y[X[:, col]<split])
-        rchild=self.fit_(X[X[:, col]>=split], y[X[:, col]>=split])
+        lchild = self.fit_(X[X[:, col]<split], y[X[:, col]<split])
+        rchild = self.fit_(X[X[:, col]>=split], y[X[:, col]>=split])
         return DecisionNode(col, split, lchild, rchild)
     
 
     def predict(self, X_test):
         """
-        Make a prediction for each record in X_test and return as array.
-        This method is inherited by RegressionTree621 and ClassifierTree621 and
-        works for both without modification!
+        Makes a prediction for each record in X_test and return as array.
+        This method is inherited by RegressionTree621 and ClassifierTree621
         """
-        tree=self.root
-        predictions=[tree.predict(x) for x in X_test]
+        tree = self.root
+        predictions = [tree.predict(x) for x in X_test]
         return predictions
 
 
@@ -122,14 +106,14 @@ class RegressionTree621(DecisionTree621):
         super().__init__(min_samples_leaf, loss=np.var)
 
     def score(self, X_test, y_test):
-        "Return the R^2 of y_test vs predictions for each record in X_test"
-        pred=self.predict(X_test)
+        "Returns the R^2 of y_test vs predictions for each record in X_test"
+        pred = self.predict(X_test)
         return r2_score(y_test, pred)
 
 
     def create_leaf(self, y):
         """
-        Return a new LeafNode for regression, passing y and mean(y) to
+        Returns a new LeafNode for regression, passing y and mean(y) to
         the LeafNode constructor.
         """
         return LeafNode(y, np.mean(y))
@@ -139,13 +123,13 @@ class ClassifierTree621(DecisionTree621):
         super().__init__(min_samples_leaf, loss=gini)
 
     def score(self, X_test, y_test):
-        "Return the accuracy_score() of y_test vs predictions for each record in X_test"
-        pred=self.predict(X_test)
+        "Returns the accuracy_score() of y_test vs predictions for each record in X_test"
+        pred = self.predict(X_test)
         return accuracy_score(y_test, pred)
 
     def create_leaf(self, y):
         """
-        Return a new LeafNode for classification, passing y and mode(y) to
-        the LeafNode constructor. Feel free to use scipy.stats to use the mode function.
+        Returns a new LeafNode for classification, passing y and mode(y) to
+        the LeafNode constructor. 
         """
         return LeafNode(y, stats.mode(y)[0])
